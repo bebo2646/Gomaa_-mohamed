f2c7098dbd4da5dda1ff6df13c33721f43f1b9ad
bebo2646 <btaha7760@gmail.com>
2026-04-10 23:37:14 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,51 +1,100 @@
-$(document).ready(function() {
-    
-    if (!$.fn.turn) {
-        console.log("Turn.js not loaded");
-        return;
-    }
-    
-    function resizeBook() {
-        var bookWidth = 800;
-        var bookHeight = 600;
+﻿$(document).ready(function() {
+    var $flipbook = $("#flipbook");
+    var pages = $flipbook.children(".page, .hard");
+    var currentPage = 1;
+    var useTurn = false;
+    var animating = false;
 
-        if ($("#flipbook").turn("is")) {
-            $("#flipbook").turn("size", bookWidth, bookHeight);
-        } else {
-            $("#flipbook").turn({
-                width: bookWidth,
-                height: bookHeight,
+    function normalizeSpread(pageNum) {
+        if (pageNum < 1) pageNum = 1;
+        if (pageNum > pages.length) pageNum = pages.length;
+        if (pageNum % 2 === 0) pageNum = pageNum - 1;
+        if (pageNum < 1) pageNum = 1;
+        if (pageNum > pages.length) pageNum = pages.length - 1;
+        return pageNum;
+    }
+
+    function showManualPage(pageNum, direction) {
+        if (animating) return;
+        pageNum = normalizeSpread(pageNum);
+        if (pageNum === currentPage) return;
+
+        var currentSpread = pages.slice(currentPage - 1, currentPage + 1);
+        var newSpread = pages.slice(pageNum - 1, pageNum + 1);
+        var $rightOld = pages.eq(currentPage - 1);
+        var $rightNew = pages.eq(pageNum - 1);
+        direction = direction || (pageNum > currentPage ? 'next' : 'prev');
+        animating = true;
+
+        currentSpread.removeClass('flip-in flip-out current').addClass('flip-out');
+        newSpread.show().addClass('current');
+        $rightNew.addClass('flip-in');
+
+        setTimeout(function() {
+            currentSpread.hide().removeClass('flip-out');
+            newSpread.removeClass('flip-in');
+            currentPage = pageNum;
+            animating = false;
+        }, 700);
+    }
+
+    if ($.fn.turn) {
+        try {
+            $flipbook.turn({
+                width: 1000,
+                height: 720,
                 autoCenter: true,
                 display: 'double',
                 acceleration: true,
                 direction: 'rtl',
-                duration: 600
+                gradients: true,
+                elevation: 60,
+                duration: 650
             });
+            useTurn = true;
             console.log("Turn.js initialized");
+        } catch (error) {
+            console.warn("Turn.js initialization failed, using manual pagination.", error);
         }
+    } else {
+        console.warn("Turn.js not loaded, using manual pagination.");
     }
 
-    resizeBook();
+    if (!useTurn) {
+        $("body").addClass("no-turn");
+        pages.hide();
+        pages.slice(0, 2).show().addClass('current');
+    }
 
-    // $(window).resize(function() {
-    //     resizeBook();
-    // });
+    $("#prevPageBtn").on("click", function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        if (useTurn) {
+            $flipbook.turn("previous");
+        } else {
+            showManualPage(currentPage - 2, 'prev');
+        }
+    });
+
+    $("#nextPageBtn").on("click", function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        if (useTurn) {
+            $flipbook.turn("next");
+        } else {
+            showManualPage(currentPage + 2, 'next');
+        }
+    });
+
+    window.goToPage = function(pageNum, event) {
+        if (event) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        if (useTurn) {
+            $flipbook.turn("page", pageNum);
+        } else {
+            showManualPage(pageNum, pageNum > currentPage ? 'next' : 'prev');
+        }
+    };
 });
-
-function goToPage(pageNum, event) {
-    event.preventDefault();
-    event.stopPropagation();
-    $("#flipbook").turn("page", pageNum);
-}
-
-function nextPage(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    $("#flipbook").turn("next");
-}
-
-function prevPage(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    $("#flipbook").turn("previous");
-}
